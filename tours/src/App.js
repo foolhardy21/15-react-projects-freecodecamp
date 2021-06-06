8931088002fb58ce0faaab8cb69391b866058b70
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2021-06-06 18:58:29 +0530

    tours - data-fetched

--- tours/src/App.js
+++ tours/src/App.js
@@ -1,12 +1,28 @@
-import React ,{useState,useEffect} from 'react'
+import React , {useState,useEffect} from 'react'
 import Loading from './Loading'
 import Tours from './Tours'
-const url = 'https://course-api.netlify.app/api/react-tours-project'
+const url = 'https://course-api.com/react-tours-project'
 
 const App = () => {
 
   const [loading,setLoading] = useState(true)
   const [tours,setTours] = useState([])
+
+  const fetchTours = async () => {
+    setLoading(true)
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false)
+      setTours(tours)
+    } catch(error) {
+      console.log(error)
+    }
+
+  }
+  useEffect(() => {
+    fetchTours();
+  },[])
 
   if(loading) {
     return (
