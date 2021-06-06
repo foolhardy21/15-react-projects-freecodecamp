b00dc444659921a46bfbde82621c5047ed082657
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2021-06-06 18:23:33 +0530

    birthday - person-component-added

--- birthday-reminder/src/components/List.js
+++ birthday-reminder/src/components/List.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Person from './Person'
 
 const List = ({people}) => {
 
@@ -6,16 +7,11 @@ const List = ({people}) => {
  return (
    <>
     {people.map(person => {
-      const {id,name,age,image} = person;
+
       return (
-        <article key={id} className='person'>
-          <img src={image} alt={name} />
-          <div>
-            <h4>{name}</h4>
-            <p>{age} Years</p>
-          </div>
-        </article>
+        <Person person={person} />
       )
+
     })}
    </>
  )
