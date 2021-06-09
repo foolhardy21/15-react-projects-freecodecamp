50b63d02f85a20930ac8d88d9a09c7601d8cfa85
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2021-06-09 14:44:58 +0530

    menu - boilerplate-plus-menulist

---
+++ menu/src/App.js
@@ -0,0 +1,24 @@
+import React, {useState} from 'react'
+import Categories from './Categories'
+import Menu from './Menu'
+import data from './data'
+
+const App = () => {
+  const [items,setItems] = useState(data)
+  const [categories,setCategories] = useState([])
+
+  return (
+    <main>
+      <section className='menu section'>
+        <div className='title'>
+          <h2>our menu</h2>
+          <div className='underline'></div>
+        </div>
+        <Categories />
+        <Menu items={items}/>
+      </section>
+    </main>
+  );
+}
+
+export default App;
---
+++ menu/src/Categories.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Categories = () => {
+  return (
+    <div>categories component</div>
+  )
+}
+
+export default Categories
