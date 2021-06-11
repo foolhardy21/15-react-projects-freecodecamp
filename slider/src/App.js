56dfeb767682f2fa3df88596c44def50916f32f5
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2021-06-11 19:30:09 +0530

    slider - boilerplate

---
+++ slider/src/App.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+import { FiChevronRight, FiChevronLeft } from 'react-icons/fi'
+import { FaQuoteRight } from 'react-icons/fa'
+import data from './data'
+
+const App = () => {
+  return (
+    <h1>hi</h1>
+  );
+}
+
+export default App;
