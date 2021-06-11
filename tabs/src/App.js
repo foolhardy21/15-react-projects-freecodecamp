2a0f89f689299b1782a33127cb6b4d57e940a71d
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2021-06-11 15:38:14 +0530

    tabs - boilerplate

---
+++ tabs/src/App.js
@@ -0,0 +1,12 @@
+import React, {useState, useEffect} from 'react'
+import {FaAngleDoublleRight} from 'react-icons/fa'
+
+const url = 'https://course-api.com/react-tabs-project'
+
+const App = () => {
+  return (
+    <div>hello</div>
+  );
+}
+
+export default App;
