455c3b3d137dbf98beb316f92ccc12aaab1a930c
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2021-06-22 15:22:55 +0530

    lorem-ipsum - structure

--- lorem-ipsum/src/App.js
+++ lorem-ipsum/src/App.js
@@ -1,9 +1,35 @@
 import React, {useState} from 'react'
+import data from './data.js'
 
 const App = () => {
 
+  const [count,setCount] = useState(0)
+  const [text,setText] = useState([])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+  const changeCount = (e) => setCount(e.target.value)
+
+
   return (
-    <h2>lorem ipsum setup</h2>
+    <section className='section-center'>
+      <h3>tired of boring lorem ipsum?</h3>
+      <form className='lorem-form' onSubmit={handleSubmit}>
+        <label className='label' htmlFor='amount'>paragraphs:</label>
+        <input
+          type='number'
+          id='amount'
+          className='input'
+          onChange={changeCount}
+          // value={count}
+        />
+        <button type='submit' className='btn'>generate</button>
+      </form>
+      <article className='lorem-text'>
+        <p>sample text kasdfsadhljdsvhj</p>
+      </article>
+    </section>
   );
 
 }
