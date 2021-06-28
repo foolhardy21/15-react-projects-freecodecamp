b91c0841ce53b7bb786839e5cb88acc496651a64
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2021-06-28 15:14:51 +0530

    color-gen - structure

--- color-gen/src/App.js
+++ color-gen/src/App.js
@@ -4,7 +4,32 @@ import Values from 'values.js'
 
 const App = () => {
 
+  const [color,setColor] = useState('')
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log('submitted')
+  }
+
+  return (
+    <div>
+      <section className='container'>
+        <h3>color generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+          type='text'
+          value={color}
+          onChange={(e) => setColor(e.target.value)}
+          placeholder='Type hex...'/>
+          <button className='btn' type='submit'>submit</button>
+        </form>
+      </section>
+      <section className='colors'>
+        <h4>List of colors</h4>
+      </section>
+
+    </div>
+  );
 }
 
 export default App;
