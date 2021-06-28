00f9fce83c1d4767967d46c117fc712e489c9409
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2021-06-28 18:26:32 +0530

    grocery-bud - boilerplate

---
+++ grocery-bud/src/App.js
@@ -0,0 +1,13 @@
+import React, {useState} from 'react'
+import Alert from './Alert'
+import List from './List'
+
+const App = () => {
+  return (
+    <div>
+    main page
+    </div>
+  );
+}
+
+export default App;
