dae69fb030eee8bed1fc8fb18d4bda99add27c43
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2021-06-29 17:09:41 +0530

    navbar - links-data

--- navbar/src/NavBar.js
+++ navbar/src/NavBar.js
@@ -16,30 +16,19 @@ const NavBar = () => {
         </div>
         <div className='links-container show-container'>
           <ul className='links'>
-            <li>
-              <a href='#'>home</a>
-            </li>
-            <li>
-              <a href='#'>about</a>
-            </li>
-            <li>
-              <a href='#'>contact</a>
-            </li>
-            <li>
-              <a href='#'>product</a>
-            </li>
+            {links.map(link => {
+              return <li key={link.id}>
+                <a href={link.url}>{link.text}</a>
+              </li>
+            })}
           </ul>
         </div>
         <ul className='social-icons'>
-          <li>
-            <a href='https://twitter.com'><FaTwitter/></a>
-          </li>
-          <li>
-            <a href='https://twitter.com'><FaTwitter/></a>
-          </li>
-          <li>
-            <a href='https://twitter.com'><FaTwitter/></a>
-          </li>
+          {social.map(site => {
+            return <li key={site.id}>
+              <a href={site.url}>{site.icon}</a>
+            </li>
+          })}
         </ul>
       </div>
     </nav>
