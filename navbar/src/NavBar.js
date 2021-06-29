a320f5fa1bf860326d62e10d6c58e723f2f8c9d2
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2021-06-29 17:19:03 +0530

    navbar - simple-toggle

--- navbar/src/NavBar.js
+++ navbar/src/NavBar.js
@@ -4,24 +4,27 @@ import {links, social} from './data'
 import logo from './logo.svg'
 
 const NavBar = () => {
+
+  const [showLinks,setShowLinks] = useState(false)
+
   return (
 
     <nav>
       <div className='nav-center'>
         <div className='nav-header'>
           <img src={logo}alt='logo' />
-          <button className='nav-toggle'>
+          <button className='nav-toggle' onClick={() => setShowLinks(!showLinks)}>
             <FaBars />
           </button>
         </div>
         <div className='links-container show-container'>
-          <ul className='links'>
+          {showLinks && <ul className='links'>
             {links.map(link => {
               return <li key={link.id}>
                 <a href={link.url}>{link.text}</a>
               </li>
             })}
-          </ul>
+          </ul>}
         </div>
         <ul className='social-icons'>
           {social.map(site => {
