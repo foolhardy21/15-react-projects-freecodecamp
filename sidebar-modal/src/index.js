aa4db372d2009ed057fb95c0a46bca555cc5b436
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2021-07-03 13:12:06 +0530

    sidebar-modal - useContext

--- sidebar-modal/src/index.js
+++ sidebar-modal/src/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { AppProvider } from './context'
 
 ReactDOM.render(
-    <App />,
+  <AppProvider>
+    <App />
+  </AppProvider>,
   document.getElementById('root')
 );
 
