f7765a547521ac38d3c72943b8f2f0705451b784
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2021-07-05 13:13:54 +0530

    stripe - boilerplate

---
+++ stripe-submenu/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavBar from './Navbar'
+import Heo from './Hero'
+import Sidebar from './Sidebar'
+import Submenu from './Submenu'
+
+const App = () => {
+  return (
+    <h2>stripe submenus setup</h2>
+  );
+}
+
+export default App;
