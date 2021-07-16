a1c9fab64549317fb0535c03b2f25a87fc0b5adc
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2021-07-16 19:36:17 +0530

    cocktails - boilerplate

---
+++ cocktails/src/components/CocktailList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Cocktail from './Cocktail'
+import Loading from './Loading'
+import { AppContext } from '../context'
+
+const CocktailList = () => {
+    return (
+        <h2>cocktail list component</h2>
+    )
+}
+
+export default CocktailList
---
+++ cocktails/src/components/SearchForm.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { AppContext } from '../context'
+
+const SearchForm = () => {
+    return (
+        <div>
+            <h2>search form component</h2>
+        </div>
+    )
+}
+
+export default SearchForm
